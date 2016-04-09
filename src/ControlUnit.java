4e0467a9fbd13ab59e10ad02afac649a9678acb2
hossamElfar <hossam.elfar@student.guc.edu.eg>
2016-04-09 18:58:35 +0300

    Done with control unit initialization

---
+++ src/ControlUnit.java
@@ -0,0 +1,83 @@
+
+public class ControlUnit {
+	
+	boolean PCSrc ;
+	boolean ALUSrc;
+	String ALUOperation;
+	boolean MemWrite;
+	boolean MemtoReg;
+	boolean MemRead;
+	boolean RegWrite;
+	
+	public ControlUnit(){
+		 this.PCSrc =false;
+		 this.ALUSrc=false;
+		 this.ALUOperation="0000";
+		 this.MemWrite =false;
+		 this.MemtoReg=false;
+		 this.MemRead=false;
+		 this.RegWrite=false;
+	}
+
+	public boolean isPCSrc() {
+		return PCSrc;
+	}
+
+	public void setPCSrc(boolean pCSrc) {
+		PCSrc = pCSrc;
+	}
+
+	public boolean isALUSrc() {
+		return ALUSrc;
+	}
+
+	public void setALUSrc(boolean aLUSrc) {
+		ALUSrc = aLUSrc;
+	}
+
+	public String getALUOperation() {
+		return ALUOperation;
+	}
+
+	public void setALUOperation(String aLUOperation) {
+		ALUOperation = aLUOperation;
+	}
+
+	public boolean isMemWrite() {
+		return MemWrite;
+	}
+
+	public void setMemWrite(boolean memWrite) {
+		MemWrite = memWrite;
+	}
+
+	public boolean isMemtoReg() {
+		return MemtoReg;
+	}
+
+	public void setMemtoReg(boolean memtoReg) {
+		MemtoReg = memtoReg;
+	}
+
+	public boolean isMemRead() {
+		return MemRead;
+	}
+
+	public void setMemRead(boolean memRead) {
+		MemRead = memRead;
+	}
+
+	public boolean isRegWrite() {
+		return RegWrite;
+	}
+
+	public void setRegWrite(boolean regWrite) {
+		RegWrite = regWrite;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
